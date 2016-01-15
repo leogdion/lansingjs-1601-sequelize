97c89bb4f2cbbf3e0729f076123891f37e036616
Leo Dion <leogdion@brightdigit.com>
2016-01-14 20:05:05 -0500

    writes to database

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('database', 'username', 'password', {
+  dialect : 'sqlite',
+  storage : 'database.sqlite'
+});
+
+var User = sequelize.define('User', {
+  username: Sequelize.STRING,
+  birthday: Sequelize.DATE
+});
+
+sequelize.sync().then(function() {
+  return User.create({
+    username: 'janedoe',
+    birthday: new Date(1980, 6, 20)
+  });
+}).then(function(jane) {
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
 });
